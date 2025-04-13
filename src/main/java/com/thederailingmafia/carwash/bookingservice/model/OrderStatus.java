e4b4d24fa61a93d65ff9ee490037cce28a32253e
nitishsingh877 <nitishkumarsingh877@gmail.com>
2025-04-13 21:07:08 +0530

    Add: added code for implmetation of rabbit mq and show data in console

--- src/main/java/com/thederailingmafia/carwash/bookingservice/model/OrderStatus.java
+++ src/main/java/com/thederailingmafia/carwash/bookingservice/model/OrderStatus.java
@@ -1,5 +1,5 @@
 package com.thederailingmafia.carwash.bookingservice.model;
 
 public enum OrderStatus {
-    PENDING, ASSIGNED, ACCEPTED, CANCELED,COMPLETED
+    PENDING, ASSIGNED, ACCEPTED, CANCELED,COMPLETED,FAILED,REJECTED,NOT_COMPLETED;
 }
